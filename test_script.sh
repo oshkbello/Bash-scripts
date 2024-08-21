283d22e78d3d17e88834a0089ce2d4cb9f8ca9d4
Oshioke D. Bello <oshkbello@gmail.com>
2024-08-21 14:21:56 -0700

    New file variables.sh and modified test_scripts.sh by adding comments

--- test_script.sh
+++ test_script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# these are comments
+#Both of these lines will be ingnored by the interpreter
 echo "This is my first Bash script"
 
 echo -e "\nI had to first set the permission for user to execute"
---
+++ variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Assigning variables
+country=Nigeria
+echo $country
+
+#Assign value based on output form a program or cmd using command substitution. 
+# $ is required to access an existing variables value
+# to access the variable value, append $ to the varibale name
+new_country=$country
+echo $new_country
+
+
